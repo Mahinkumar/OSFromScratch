3ee67ed95688190f53a673395f261ac0fe7142d1
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2025-01-14 15:02:04 +0530

    Update docs

--- run.sh
+++ run.sh
@@ -3,14 +3,15 @@ set -xue
 
 QEMU=qemu-system-riscv64
 
-# new: Path to clang and compiler flags
+# Path to clang and compiler flags
+# mcmodel compiler flag to setup medium-any code model.
 CC=clang 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv64 -ffreestanding -nostdlib -mcmodel=medany"
 
-# new: Build the kernel
+# Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-    -kernel kernel.elf # new: Load the kernel
+    -kernel kernel.elf #Load the kernel
